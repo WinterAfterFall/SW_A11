1ae6279fa9c6f94f465d775626fc2c3940b93942
tonnam <tonnam.jit@gmail.com>
2025-04-24 14:02:48 +0700

    s

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-onst dayOfTheWeek = (date = new Date()) => {
+const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
