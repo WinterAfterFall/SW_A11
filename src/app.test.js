4719967c31eaff41e6a5077ca505c92a4e2fd75f
tonnam <tonnam.jit@gmail.com>
2025-04-24 13:58:48 +0700

    s

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,14 @@
+ใช้พื้นที่เก็บข้อมูลไป 92% … หากพื้นที่เก็บข้อมูลเต็ม คุณจะไม่สามารถบันทึกลงในไดรฟ์ สำรองข้อมูลไปยัง Google Photos หรือใช้ Gmail รับพื้นที่เก็บข้อมูลขนาด 100 GB ในราคา ฿70.00 ฿17.00/เดือน เป็นเวลา 3 เดือน
+app.test.js
 const { dayOfTheWeek } = require('./app');
+const { monthOfTheYear } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
+});
+test('getDay returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('March');
 });
